c6366f22e7d5cdd0018c216c2aa8e441fd1e3f95
Iti Singh <98singh.iti@gmail.com>
2020-12-04 12:05:01 +0530

    first commit

---
+++ app/src/main/java/com/iti/activitylifecycle/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.iti.activitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("onCreate called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy called ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop called")
+    }
+}
